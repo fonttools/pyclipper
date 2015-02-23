2a41c7f03e9ee1e27f433a6ae112cfab2a94f719
Gregor R <me@gregorratajc.com>
2015-02-23 10:19:50 +0100

    Updated setup.py file for new file organization

--- setup.py
+++ setup.py
@@ -2,20 +2,18 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-  
 
-ext =	Extension("pyclipper", 
-                sources=["pyclipper.pyx", "clipper.cpp"],
-	          	libraries=["stdc++"], # "ln", "util" ,"pthread" ,"rt" 
-                language="c++",              # this causes Pyrex/Cython to create C++ source
-				library_dirs=["/Library/Python/2.7/site-packages"],
-				include_dirs=["./"],
-				)
-
+ext = Extension("pyclipper.pyclipper",
+                sources=["pyclipper/pyclipper.pyx", "pyclipper/clipper.cpp"],
+                libraries=["stdc++"],  # "ln", "util" ,"pthread" ,"rt"
+                language="c++",  # this causes Pyrex/Cython to create C++ source
+                library_dirs=["/Library/Python/2.7/site-packages"],
+                include_dirs=["./"]
+)
 
 setup(
-        ext_modules=[ext],
-        cmdclass = {'build_ext': build_ext},
+    ext_modules=[ext],
+    cmdclass={'build_ext': build_ext},
 )
 
 
