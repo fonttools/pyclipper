01b0ee4193c6ca213a7e24353ffc6d03310f03e7
Gregor R <me@gregorratajc.com>
2015-02-24 13:32:41 +0100

    Migrated to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
-from distutils.core import setup, Command
-from distutils.extension import Extension
+import sys
+from setuptools import setup
+from setuptools.extension import Extension
+from setuptools.command.test import test as TestCommand
 
 try:
     from Cython.Distutils import build_ext
@@ -22,21 +24,23 @@ ext = Extension("pyclipper",
 )
 
 
-class PyTest(Command):
-    user_options = []
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
     def initialize_options(self):
-        pass
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
 
     def finalize_options(self):
-        pass
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-    def run(self):
-        import subprocess
-        import sys
-
-        errno = subprocess.call([sys.executable, 'runtests.py'])
-        raise SystemExit(errno)
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -99,6 +103,7 @@ Source: `Angus Johnson\'s Clipper library
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     ext_modules=[ext],
+    tests_require=['pytest'],
     cmdclass={
         'test': PyTest,
         'build_ext': build_ext},
