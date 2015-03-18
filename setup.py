f2da931f9de10977e22fb0227a2d5740387a9757
Gregor R <me@gregorratajc.com>
2015-03-18 16:04:36 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from setuptools.extension import Extension
 from setuptools.command.test import test as TestCommand
 
-version = '0.8.dev2'
+version = '0.9.beta'
 
 """
 Note on using the setup.py:
