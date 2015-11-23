bb76d0f84dd51786883c6b47ed1c31ddfed34f13
Gregor Ratajc <gregor.ratajc@zemanta.com>
2015-11-23 21:48:31 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from setuptools.extension import Extension
 from setuptools.command.test import test as TestCommand
 
-version = '1.0.0'
+version = '1.0.1'
 
 """
 Note on using the setup.py:
